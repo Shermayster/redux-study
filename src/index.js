1cfc043c8d0c58e541ab94555500f270cfafe441
pavelshermayster <sherm84@gmail.com>
2017-09-09 09:25:34 +0300

    redux commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store';
 
-const state = {
-    todos: [
-        {id: 1, name: 'Render static UI', isComplete: true},
-        {id: 2, name: 'Create initial state', isComplete: true},
-        {id: 3, name: 'Render based on state', isComplete: true}
-    ]
+const todoChangeHandler = (val) => store.dispatch(
+    {type: 'CURRENT_UPDATE', payload: val}
+)
+
+const render = () => {
+    const state = store.getState();
+    ReactDOM.render(<App 
+        todos={state.todos} 
+        currentTodo={state.currentTodo}
+        changeCurrent={todoChangeHandler}
+        />, document.getElementById('root'));
 }
-ReactDOM.render(<App todos={state.todos}/>, document.getElementById('root'));
+render();
+
+store.subscribe(render);
+
 registerServiceWorker();
---
+++ src/reducers/todo.js
@@ -0,0 +1,20 @@
+const initState = {
+    todos: [
+        {id: 1, name: 'Render static UI', isComplete: true},
+        {id: 2, name: 'Create initial state', isComplete: false},
+        {id: 3, name: 'Render based on state', isComplete: false}
+    ],
+    currentTodo: ''
+}
+export default (state = initState, action) => {
+    switch(action.type) {
+        case 'TODO_ADD':
+        return {...state, todos: state.todos.concat(action.payload)}
+        break;
+        case 'CURRENT_UPDATE':
+        return {...state, currentTodo: action.payload}
+        default:
+        return state;
+    }
+    
+}
